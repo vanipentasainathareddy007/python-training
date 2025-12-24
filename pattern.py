c49c5d0d7dbde00dec763f05e8faf71fcdc5de18
vanipentasainathareddy007 <vanipentasainath@gmail.com>
2025-12-24 12:03:03 +0530

    Update pattern.py

--- pattern.py
+++ pattern.py
@@ -1,4 +1,5 @@
-'''x=int(input())
+#1.fizzbuzz
+x=int(input())
 for i in range (1,x+1):
     if((i%3==0)&(i%5==0)):
         print('fizz buzzz')
@@ -7,9 +8,60 @@ for i in range (1,x+1):
     elif(i%5==0):
         print('buzz')
     else:
-        print(i)'''
+        print(i)
+2.pattern printing.
+#a.square
 x=int(input())
 for  i in range(x):
     for j in range(x):
         print("*",end=" ")
     print("\n")
+#b.right angled
+x=int(input())
+for  i in range(x):
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
+#c.right angled in numbers
+x=int(input())
+for  i in range(x):
+    for j in range(i+1):
+        print(j+1,end=" ")
+    print()
+#d.pyramid
+x=int(input())
+for  i in range(x):
+    for j in range(x-i-1):
+        print(" ",end=" ")
+    for j in range(i+1):
+        print("*  ",end=" ")
+    print("\n")
+#4.diamond
+x=int(input())
+for  i in range(x):
+    for j in range(x-i-1):
+        print(" ",end=" ")
+    for j in range(i+1):
+        print("*  ",end=" ")
+    print("\n")
+for  i in range(x-2,-1,-1):
+    for j in range(x-i-1):
+        print(" ",end=" ")
+    for j in range(i+1):
+        print("*  ",end=" ")
+    print("\n")
+#5.odd sequence
+x=int(input())
+for  i in range(x+4):
+    for j in range(x-i+4):
+        print(" ",end=" ")
+    for j in range(i+1):
+         if(i%2!=0):
+            break
+         else:
+            print("*  ",end=" ")
+    print()
+
+        
+        
+
